66b6c8b41b24f603ecc442bb335be20bd1a8533d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-04 23:09:03 +0600

    okay

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -12,7 +12,7 @@ const Form = () => {
         createUserWithEmailAndPassword(auth, email, password)
         .then(result =>{
             const user = result.user;
-            console.log(user)
+            console.log('user',user)
         })
         .catch(error =>{
             console.error(error)
