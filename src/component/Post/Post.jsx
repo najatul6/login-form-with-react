b6030a09b8e23342cae5a61ecf50085832d178bf
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-04 23:22:06 +0600

    Change body color

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -7,8 +7,8 @@ const Post = ({ post }) => {
         <div className="p-5">
             <div className="border h-full p-2 space-y-2 bg-blue-400 rounded-xl drop-shadow-2xl">
                 <h2 className="text-lg font-medium text-green-600">Id No : {userId}</h2>
-                <h2 className="text-lg font-medium text-[#747]">Title : {title}</h2>
-                <p className="text-white">Details : {body}</p>
+                <h2 className="text-lg font-medium ">Title : <span className='text-[#747]'>{title}</span></h2>
+                <p className="text-white">Details : <span className="text-[#501fb0]">{body}</span></p>
             </div>
         </div>
     );
