abd52bb677cedee832cf37a61dda2ae8525ca9e3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-02-04 23:31:57 +0600

    change card shape

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -4,11 +4,11 @@ const Post = ({ post }) => {
     const { userId, title, body } = post;
 
     return (
-        <div className="p-5">
-            <div className="border h-full p-2 space-y-2 bg-[#DC4D01] rounded-xl drop-shadow-2xl">
-                <h2 className="text-lg font-medium text-white">Id No : <span className='text-green-600'>{userId}</span></h2>
-                <h2 className="text-lg font-medium text-white">Title : <span className='text-[#747]'>{title}</span></h2>
-                <p className="text-white">Details : <span className="text-[#501fb0]">{body}</span></p>
+        <div className="px-5">
+            <div className="h-full px-3 py-2 space-y-2 bg-[#319cf4] rounded-xl shadow-2xl shadow-black">
+                <h2 className="text-lg font-bold text-white capitalize ">Id No : <span className='text-[#bd1a0e88]'>{userId}</span></h2>
+                <h2 className="text-lg font-bold text-white capitalize">Title : <span className='text-[#4534ff]'>{title}</span></h2>
+                <p className="text-white capitalize font-bold">Details : <span className="text-[#3ad4ff]">{body}</span></p>
             </div>
         </div>
     );
